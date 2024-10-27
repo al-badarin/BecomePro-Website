b43e55ca8cd793b7b4f991f07811b5f097f09914
al-badarin <jamalalbadarin@gmail.com>
2024-10-27 18:07:05 +0200

    add routing

--- becomepro-website/src/App.jsx
+++ becomepro-website/src/App.jsx
@@ -1,9 +1,24 @@
-import { useState } from 'react';
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './pages/Home';
 
 function App() {
-  return <>
-  
-  </>;
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/grip-socks/features" element={<div>Характеристики</div>} />
+        <Route path="/grip-socks/pricing" element={<div>Цени</div>} />
+        <Route path="/grip-socks/order" element={<div>Поръчай сега</div>} />
+        <Route path="/grip-socks/reviews" element={<div>Отзиви</div>} />
+        <Route path="/training/fitness" element={<div>Фитнес режими</div>} />
+        {/* Add other routes for training services as needed */}
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
