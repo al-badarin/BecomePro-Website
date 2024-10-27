bae270cab780c927961287933278232b8f24c270
al-badarin <jamalalbadarin@gmail.com>
2024-10-27 18:07:21 +0200

    Create Home.jsx

---
+++ becomepro-website/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+// src/pages/Home.jsx
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Добре дошли в BecomePro</h1>
+      <p>Вашата платформа за футболни тренировки и аксесоари.</p>
+    </div>
+  );
+};
+
+export default Home;
