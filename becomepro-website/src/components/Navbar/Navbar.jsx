2bc9cd1e80dba5c97f9f24bb2a2fbed28d79e07e
al-badarin <jamalalbadarin@gmail.com>
2024-10-29 11:54:21 +0200

    Update Navbar.jsx

--- becomepro-website/src/components/Navbar/Navbar.jsx
+++ becomepro-website/src/components/Navbar/Navbar.jsx
@@ -22,37 +22,128 @@ const Navbar = () => {
         {isMenuOpen ? <FaTimes /> : <FaBars />}
       </div>
       <ul className={`${styles.navLinks} ${isMenuOpen ? styles.active : ''}`}>
-        <li className={styles.home}><Link to="/" onClick={() => setMenuOpen(false)}>НАЧАЛО</Link></li>
+        <li className={styles.home}>
+          <Link to="/" onClick={() => setMenuOpen(false)}>
+            НАЧАЛО
+          </Link>
+        </li>
+
+        {/* GRIP SOCKS Menu */}
         <li
           onMouseEnter={() => setGripSocksOpen(true)}
           onMouseLeave={() => setGripSocksOpen(false)}
+          className={styles.dropdownContainer}
         >
-          <span>GRIP SOCKS</span>
+          <Link to="/grip-socks" onClick={() => setMenuOpen(false)}>
+            GRIP SOCKS
+          </Link>
           {isGripSocksOpen && (
             <ul className={styles.dropdown}>
-              <li><Link to="/grip-socks/features" onClick={() => setMenuOpen(false)}>Характеристики</Link></li>
-              <li><Link to="/grip-socks/pricing" onClick={() => setMenuOpen(false)}>Цени</Link></li>
-              <li><Link to="/grip-socks/order" onClick={() => setMenuOpen(false)}>Поръчай сега</Link></li>
-              <li><Link to="/grip-socks/reviews" onClick={() => setMenuOpen(false)}>Отзиви</Link></li>
+              <li>
+                <Link
+                  to="/grip-socks/features"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Характеристики
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/grip-socks/pricing"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Цени
+                </Link>
+              </li>
+              <li>
+                <Link to="/grip-socks/order" onClick={() => setMenuOpen(false)}>
+                  Поръчай сега
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/grip-socks/reviews"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Отзиви
+                </Link>
+              </li>
             </ul>
           )}
         </li>
+
+        {/* TRAINING SERVICES Menu */}
         <li
           onMouseEnter={() => setTrainingOpen(true)}
           onMouseLeave={() => setTrainingOpen(false)}
+          className={styles.dropdownContainer}
         >
           <span>ТРЕНИРОВЪЧНИ УСЛУГИ</span>
           {isTrainingOpen && (
             <ul className={styles.dropdown}>
-              <li><Link to="/training/fitness" onClick={() => setMenuOpen(false)}>Фитнес режими</Link></li>
-              <li><Link to="/training/technical" onClick={() => setMenuOpen(false)}>Технически програми</Link></li>
-              <li><Link to="/training/mental" onClick={() => setMenuOpen(false)}>Ментална подготовка</Link></li>
-              <li><Link to="/training/nutrition" onClick={() => setMenuOpen(false)}>Хранене и хранителни режими</Link></li>
-              <li><Link to="/training/speed" onClick={() => setMenuOpen(false)}>Скорост и експлозивност</Link></li>
-              <li><Link to="/training/recovery" onClick={() => setMenuOpen(false)}>Възстановяване</Link></li>
-              <li><Link to="/training/mobility" onClick={() => setMenuOpen(false)}>Мобилност и Стречинг</Link></li>
-              <li><Link to="/training/endurance" onClick={() => setMenuOpen(false)}>Издръжливост</Link></li>
-              <li><Link to="/training/video-analysis" onClick={() => setMenuOpen(false)}>Видео Анализи</Link></li>
+              <li>
+                <Link to="/training/fitness" onClick={() => setMenuOpen(false)}>
+                  Фитнес режими
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/training/technical"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Технически програми
+                </Link>
+              </li>
+              <li>
+                <Link to="/training/mental" onClick={() => setMenuOpen(false)}>
+                  Ментална подготовка
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/training/nutrition"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Хранене и хранителни режими
+                </Link>
+              </li>
+              <li>
+                <Link to="/training/speed" onClick={() => setMenuOpen(false)}>
+                  Скорост и експлозивност
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/training/recovery"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Възстановяване
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/training/mobility"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Мобилност и Стречинг
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/training/endurance"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Издръжливост
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/training/video-analysis"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  Видео Анализи
+                </Link>
+              </li>
             </ul>
           )}
         </li>
