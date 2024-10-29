fb3746c643462233b1a378e16ccd78528a7dad21
al-badarin <jamalalbadarin@gmail.com>
2024-10-29 11:54:17 +0200

    Update App.jsx

--- becomepro-website/src/App.jsx
+++ becomepro-website/src/App.jsx
@@ -3,10 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Home from './pages/Home';
-import Features from './pages/GripSocks/Features/Features';
-import Pricing from './pages/GripSocks/Pricing/Pricing';
-import OrderForm from './pages/GripSocks/OrderForm/OrderForm';
-import Reviews from './pages/GripSocks/Testimonials/Testimonials';
+import GripSocksPage from './pages/GripSocks/GripSocksPage';
 
 function App() {
   return (
@@ -14,10 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/grip-socks/features" element={<Features />} />
-        <Route path="/grip-socks/pricing" element={<Pricing />} />
-        <Route path="/grip-socks/order" element={<OrderForm />} />
-        <Route path="/grip-socks/reviews" element={<Reviews />} />
+        <Route path="/grip-socks" element={<GripSocksPage />} />
       </Routes>
     </Router>
   );
